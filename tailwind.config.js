0ef4d793a3a22f51a2f46eb457074558006360cd
vechicin <santiagovelosa@gmail.com>
2025-03-18 08:27:31 -0500

    Add brandbook colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'primary-blue': '#33ACFD',
+                'primary-white': '#0000',
+                'secondary-gray': '#A2AFB7',
+                'secondary-blue': '#002338',
+                'secondary-orange': '#FE9130'
+            }
+        },
     },
     plugins: [],
 };
