37d522415197beda68095958565bbc107d4487bf
vechicin <santiagovelosa@gmail.com>
2025-05-06 07:35:45 -0500

    Update Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/devros-front/",
+  base: "/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
