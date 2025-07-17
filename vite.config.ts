20fc41cfe19d4eade24a5516cde63ef81b81b49d
vechicin <santiagovelosa@gmail.com>
2025-07-17 18:00:23 -0500

    Install dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import yaml from '@modyfi/vite-plugin-yaml';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    yaml()
+  ],
   base: "/",
   optimizeDeps: {
     exclude: ['lucide-react'],
